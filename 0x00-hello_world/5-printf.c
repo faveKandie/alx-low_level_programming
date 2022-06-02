4117cc9be545402ae5ada0ef81b3c84082fe5f0e
faveKandie <obianujuofili03@gmail.com>
2022-06-02 06:28:08 -0700

    5printed commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 //this program will print  with proper grammar, but the outcome is a piece of art//
+int main(void)
 {
 	printf ("with proper grammar, but the outcome is a piece of art, \n");
 	return (0);
