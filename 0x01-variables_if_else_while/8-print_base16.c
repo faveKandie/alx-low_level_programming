abd33ecf387fc42e1294d3eacfb628b8495f926a
faveKandie <obianujuofili03@gmail.com>
2022-06-04 08:12:49 -0700

    french commit

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,9 +9,13 @@ int main(void)
 	char letter;
 
 	for (letter = '0'; letter <= '9'; letter++)
+	{
 	       putchar(letter);
+	}
 for (letter = 'a'; letter <= 'f'; letter++)
+{
 putchar(letter);
+}
 putchar('\n');
 return (0);
 }
