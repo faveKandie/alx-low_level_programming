098de9c3764113eeaf1bf77b54d75f728fb08b62
faveKandie <obianujuofili03@gmail.com>
2022-06-04 05:17:51 -0700

    juju cpmmit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,11 +5,11 @@
  * main - Entry point
  * Return: Always 0 Success
  */
-int main(void);
+int main(void)
 {
 	int n;
 	srand(time(0));
-	n = rand() - RAND_MAX / 2
+	n = rand() - RAND_MAX / 2;
 		if (n > 0)
 			printf("%d is positive\n", n);
 		else if (n < 0)
