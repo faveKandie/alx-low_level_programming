90e29940f9f7580cfa855ac52188fbec0bdedcc3
faveKandie <obianujuofili03@gmail.com>
2022-06-04 07:07:06 -0700

    putchar commit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Retrun: Always 0 Success
+ */
+int main(void)
+{
+	int num;
+
+	for (num = '0'; num <= '10'; num++)
+		putchar(num);
+	putchar('\n');
+	return (0);
+}
