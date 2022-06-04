04f68b56eb0dcd8ce48351bc85272d52f00df476
faveKandie <obianujuofili03@gmail.com>
2022-06-04 07:49:46 -0700

    baby commit

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,7 +9,7 @@ int main(void)
 	int num;
 	char letter;
 
-	for (num = 0; num <= 9; num++)
+	for (num = '0'; num <= '9'; num++)
 	       putchar(num);
 for (letter = 'a'; letter <= 'f'; letter++)
 putchar(letter);
