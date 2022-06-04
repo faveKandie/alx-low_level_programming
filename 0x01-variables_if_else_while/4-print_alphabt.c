0ad1e993cc38e4e8bda870cb7e98352e352bc717
faveKandie <obianujuofili03@gmail.com>
2022-06-04 06:38:53 -0700

    wisdom commit

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 Success
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter != 'q'; letter != 'e'; letter++)
+		putchar(letter);
+	putchar('\n');
+	return (0);
+}
