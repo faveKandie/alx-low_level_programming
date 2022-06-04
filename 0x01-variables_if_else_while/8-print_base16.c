cf84cf8a700d9441685985639dfdfc2d0a84ec4f
faveKandie <obianujuofili03@gmail.com>
2022-06-04 08:03:22 -0700

    tommy commit

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -10,7 +10,7 @@ int main(void)
 	char letter;
 
 	for (num = '0'; num <= '9'; num++)
-	       putchar((num);
+	       putchar(num);
 for (letter = 'a'; letter <= 'f'; letter++)
 putchar(letter);
 putchar('\n');
