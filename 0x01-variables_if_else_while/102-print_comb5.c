a007526627465cce1983669436b7bca967fa7280
faveKandie <obianujuofili03@gmail.com>
2022-06-04 11:12:46 -0700

    richly commit

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,7 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 /**
- * main
+ * main - Entry point
  * Return: Always 0 Success
  */
 int main(void)
@@ -13,11 +13,11 @@ int main(void)
 	{
 		for (num2 = num1 + 1; num2 <= 99; num2++)
 		{
-			putchar(num1 / 10);
-			putchar(num1 % 10);
+			putchar((num1 / 10) + '0');
+			putchar((num1 % 10) + '0');
 			putchar(' ');
-			putchar(num2 / 10);
-			putchar(num2 % 10);
+			putchar((num2 / 10) + '0');
+			putchar((num2 % 10) + '0');
 
 			if (num1 == 98 && num2 == 99)
 				continue;
