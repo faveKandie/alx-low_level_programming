492c8d0129437fa9ad9d1a575462c869674c462a
faveKandie <obianujuofili03@gmail.com>
2022-06-04 10:11:24 -0700

    compoundcommit

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 Success
+ */
+int main(void)
+{
+	int num1;
+	int num2;
+
+	for (num1 = '0'; num1 <= '9', num1++)
+	{
+		for (num2 = num1 + 1; num2 <= '9'; num2++)
+		{
+			if (num1 != num2)
+			{
+				putchar(num1);
+				putchar(num2);
+				if (d == '8' && p == '9')
+					putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
