58ac394eccffc6e48c05d894c4ef043dd0190474
faveKandie <obianujuofili03@gmail.com>
2022-06-07 12:31:25 -0700

    added commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * add - Entry point
+ * Return: Always result Success
+ */
+int add(int, int)
+{
+	int num1, num2, result;
+
+	for (num1 = 0; num1 <= 9; num1++)
+	{
+		for (num2 = 1; num2 <= 9; num2++)
+		{
+			result = num1 + num2;
+			if ((result / 10) >= 0)
+				_putchar((result / 10) + '0');
+			else
+				_putchar(' /);
+			_putchar((result % 10) + '0');
+		}
+		_putchar('\n');
+		return (result);
+	}
+}
