88377c389c8468ec27d1243c0b515ffb09f75980
faveKandie <obianujuofili03@gmail.com>
2022-06-07 10:13:27 -0700

    goat commit

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,7 +1,8 @@
 #include "main.h"
 /**
  * jack_bauer - write a function that prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59
- * @a:  the letter being used
+ * @hour: parameter being used
+ * @minute: another parameter being used
  * Return: Always 0 Success
  */
 void jack_bauer(void)
