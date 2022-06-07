19b19cd95cccdb63dd97e47569729caefbe77b20
faveKandie <obianujuofili03@gmail.com>
2022-06-07 15:39:49 -0700

    franchise

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdlio.h>
 /**
  * print_to_98 - Entry ruote
  * @n: parameters to be used
@@ -9,11 +10,11 @@ void print_to_98(int n)
 	if (n <= 98)
 			{
 						for (n = n; n <= 97; n++)
-										printf("%d, ", n);
+										printf("%d, ");
 							}
 		else
 					for (n = n; n > 98; n--)
-									printf("%d, ", n);
+									printf("%d, ");
 			printf("98\n");
 
 }
