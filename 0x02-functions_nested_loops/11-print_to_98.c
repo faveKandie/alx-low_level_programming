a385b077c7fd59f3494d3260b00dbea439b2d964
faveKandie <obianujuofili03@gmail.com>
2022-06-07 15:07:18 -0700

    jericho commit

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
  * print_to_98
+ * @n: parameters to be used
  * Return: Always 0 Success
  */
 void print_to_98(int n)
 {
-	for (n = 0; n<= 98; n++)
+	for (n = 0; n <= 98; n++)
 	{
-		_putchar('0');
 		_putchar(',');
 		_putchar(' ');
 		 if ((n /10) <= 98)
