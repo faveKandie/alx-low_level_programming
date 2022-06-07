f3ab5ded353fb51fdfb9c1ab1766075732fa8d2b
faveKandie <obianujuofili03@gmail.com>
2022-06-07 08:57:15 -0700

    getout commit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -4,14 +4,14 @@
  * @a: number being used
  * Return: Always 0 Success
  */
-int print_last_digit(int)
+int print_last_digit(int a)
 {
 	int a;
 	int result;
 	result = a % 10;
 
 	if (result < a)
-		result *= -1
+		result *= -1;
 
 			_putchar(result + '0');
 	return (0);
