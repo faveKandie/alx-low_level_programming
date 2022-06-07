47b37fe8f6ce6a15e2073207e7dcd60c1612c152
faveKandie <obianujuofili03@gmail.com>
2022-06-07 05:42:07 -0700

    slower commit

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,22 @@
+#innclude "main.h"
+/**
+ * _islower - Returns one if c is lowercase
+ * Return: 1 and 0 Success
+ */
+int _islower(int c);
+{
+	char letter;
+
+	while (letter = 'a'; letter <= 'z'; letter++)
+	{
+		for (letter = 'A'; letter <= 'Z'; letter++)
+		{
+			_putchar(letter);
+		}
+		_putchar('\n');
+		return (0);
+	}
+	_putchar(letter);
+	_putchar('\n');
+	return (1);
+}
