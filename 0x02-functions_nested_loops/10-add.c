8aba8c95d5106241cc1975b7e79f092971e01ed7
faveKandie <obianujuofili03@gmail.com>
2022-06-07 14:31:12 -0700

    addes commit

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -8,23 +8,8 @@
 int add(int num1, int num2)
 {
 	int result;
-
-	for (num1 = 0; num1 <= 9; num1++)
-	{
-		_putchar('0');
-		for (num2 = 1; num2 <= 9; num2++)
-		{
-			_putchar(',');
-			_putchar(' ');
-
-			result = num1 + num2;
-			if ((result / 10) >= 0)
-				_putchar((result / 10) + '0');
-			else
-				_putchar(' ');
-			_putchar((result % 10) + '0');
-		}
-	}
+	result = num1 + num2;
+	
 		_putchar('\n');
 		return (result);
 }
