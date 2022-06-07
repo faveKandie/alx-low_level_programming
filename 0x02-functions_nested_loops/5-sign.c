125610674311e13cec8b97383e15030f235e0da5
faveKandie <obianujuofili03@gmail.com>
2022-06-07 07:39:18 -0700

    signed commit

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_sign - Entry point
+ * @n: the letter being tested
+ * Return: Always 0, -1 and 1 Success
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+		printf("+", n);
+	return (1);
+
+	else if (n < 0)
+		printf("-", n);
+	return (-1);
+
+	else
+		printf("0");
+	return (0);
+}
