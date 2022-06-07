a489e37a46dceef89f28c0d8ad69dbc68184d4b5
faveKandie <obianujuofili03@gmail.com>
2022-06-07 13:01:22 -0700

    adder commit

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,6 +1,8 @@
 #include "main.h"
 /**
  * add - Entry point
+ * @num1: one of the variables
+ * @num2: the second one
  * Return: Always result Success
  */
 int add(int num1, int num2)
@@ -9,8 +11,12 @@ int add(int num1, int num2)
 
 	for (num1 = 0; num1 <= 9; num1++)
 	{
+		_putchar('0');
 		for (num2 = 1; num2 <= 9; num2++)
 		{
+			_putchar(',');
+			_putchar(' ');
+
 			result = num1 + num2;
 			if ((result / 10) >= 0)
 				_putchar((result / 10) + '0');
