a5d62b18ebabd6a0b80d222630bf19621faf50dc
faveKandie <obianujuofili03@gmail.com>
2022-06-07 15:11:16 -0700

    marny commit

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,16 +1,19 @@
 #include "main.h"
+#include <stdlib.h>
 /**
- * print_to_98
+ * print_to_98 - Entry ruote
+ * main - Entry point
  * @n: parameters to be used
  * Return: Always 0 Success
  */
+int main(void)
 void print_to_98(int n)
 {
 	for (n = 0; n <= 98; n++)
 	{
 		_putchar(',');
 		_putchar(' ');
-		 if ((n /10) <= 98)
+		 if ((n / 10) <= 98)
 			 _putchar((n / 10) + '0');
 		 else
 			 _putchar(' ');
