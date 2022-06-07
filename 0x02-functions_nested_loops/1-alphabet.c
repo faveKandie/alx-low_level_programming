e98b2796b9ebe2ff6941dbd803010b8c1ee1c973
faveKandie <obianujuofili03@gmail.com>
2022-06-07 04:18:58 -0700

    added commit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <main.h>
+#include "main.h"
 /**
  * main - Entry Write a function that prints the alphabet, in lowercase, followed by a new line.
  * Return: Always 0 Success
