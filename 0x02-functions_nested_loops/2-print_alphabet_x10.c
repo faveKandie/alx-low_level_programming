e39ebdc57c529a24bede38d515d4bb08b63fb487
faveKandie <obianujuofili03@gmail.com>
2022-06-07 05:28:56 -0700

    gator commit

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,7 +3,7 @@
  * print_alphabet_x10 - prints ten times the alphabet in lowercase
  * Return: Always 0 Success
  */
-void print_alphabet_x10(void);
+void print_alphabet_x10(void)
 {
 	int count = 0;
 	char letter;
