bd669034af0c2b310804100ffcebd0dfaa489df7
faveKandie <obianujuofili03@gmail.com>
2022-06-07 11:25:56 -0700

    riches commit

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * times_table - Entry point
+ * Return: Always 0 Success
+ */
+void times_table(void)
+{
+	int num, digit, result;
+	int result = num * digit;
+
+	for (num = 0; num <= 9; num++)
+	{
+		for (digit = 0; digit <= 9; digit++)
+		{
+			_putchar('0');
+			_putchar(',');
+			_putchar(' ');
+			{
+				if ((result / 10) > 0)
+				{
+					_putchar((result / 10) + '0');
+					else
+					{
+						_putchar(' ');
+					}
+					_putchar((result %10) + '0');
+				}
+				_putchar('\n');
+			}
+		}
+	}
+}
