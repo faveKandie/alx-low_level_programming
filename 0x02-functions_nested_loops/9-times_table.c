4a63964c61c1c33ceb5969dab3ca869a03ccd37f
faveKandie <obianujuofili03@gmail.com>
2022-06-07 11:42:39 -0700

    cheese commit

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,9 +9,9 @@ void times_table(void)
 
 	for (num = 0; num <= 9; num++)
 	{
+		_putchar('0');
 		for (digit = 0; digit <= 9; digit++)
 		{
-			_putchar('0');
 			_putchar(',');
 			_putchar(' ');
 			{
@@ -25,7 +25,7 @@ void times_table(void)
 					{
 						_putchar(' ');
 					}
-					_putchar((result %10) + '0');
+					_putchar((result % 10) + '0');
 				_putchar('\n');
 			}
 		}
