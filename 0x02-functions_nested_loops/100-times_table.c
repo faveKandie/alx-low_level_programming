cae654e3dff6131f8771d8f2308b6792ea518901
faveKandie <obianujuofili03@gmail.com>
2022-06-07 18:03:02 -0700

    yama

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * print_times_table - Entry point
+ * Return: Allways 0 Success
+ */
+void print_times_table(int n)
+{
+	int digit, result;
+
+	for (n = 0; n <= 15; n++)
+	{
+		_putchar('0');
+		for (digit = 1; digit <= 15; digit++)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			result = n * digit;
+			if ((result / 10) > 0)
+				_putchar((result / 10) + '0');
+			else 
+				_putchar(' ');
+			_putchar((result % 10) + '0');
+
+			if (digit < 15)
+				_putchar(',');
+			_putchar(' ');
+		}
+		_putchar('\n');
+	}
+}
