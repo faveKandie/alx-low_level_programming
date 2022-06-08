7b69a8b71db83cbea7379d72685080ecbe34fb02
faveKandie <obianujuofili03@gmail.com>
2022-06-08 03:56:00 -0700

    obeni

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - check the code for Holberton School students
+ * Return: Always 0
+ */
+int main(void)
+{
+	int a = 1, b = 2, num = 0, total = 0, i;
+
+	for (i = 0; b < 4000000; i++)
+	{
+		if (i % 2 == 0)
+			total = total + b;
+		num = a + b;
+
+		a = b;
+		b = num;
+	}
+
+	printf("%d\n", total);
+	return(0);
+}
