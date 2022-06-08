7f192eb304587cb6dde399124740c201d50770c3
faveKandie <obianujuofili03@gmail.com>
2022-06-07 17:06:22 -0700

    freshing

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,7 +11,11 @@ void print_to_98(int n)
 	{
 		while (n <= 98)
 		{
-			printf("%d, ", n);
+			printf("%d", n);
+			if (n != 98)
+			{
+				printf(", ");
+			}
 			n++;
 		}
 	}
@@ -19,12 +23,17 @@ void print_to_98(int n)
 	{
 		while (n >= 98)
 		{
-			printf("%d ", n);
+			printf("%d", n);
+			if (n != 98)
+			{
+				printf(", ");
+			}
 			n--;
 		}
 	}
 	else
 	{
-		printf("98\n");
+		printf("98");
 	}
+	printf("\n");
 }
