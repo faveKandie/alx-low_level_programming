8fafbb1bbf8708b234f48372813bfa9633447788
faveKandie <obianujuofili03@gmail.com>
2022-06-08 02:48:16 -0700

    trials

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+
+	printf("Infinite loop incoming :(\n");
+
+	i = 0;
+
+	printf("Infinite loop avoided! \\o/\n");
+	return (0);
+}
