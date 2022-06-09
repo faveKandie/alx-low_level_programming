a8c61ba28ac795d1648cc0923e007bbfac120e2c
faveKandie <obianujuofili03@gmail.com>
2022-06-09 07:09:03 -0700

    ca

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -5,12 +5,19 @@
  */
 void more_numbers(void)
 {
-	int n;
+	int n, m;
 
-	for (n =0; n <= 14; n++)
+	for (m = 0; m <= 9; m++)
 	{
-		_putchar((n / 10) + '0');
+		for (n = 0; n <= 14; n++)
+		{
+			if (n > 9)
+			{
+				_putchar((n / 10) + '0');
+				_putchar((n % 10) + '0');
+			}
+
+			_putchar('\n');
+		}
 	}
-	_putchar((n % 10) + '0');
-	_putchar('\n');
 }
