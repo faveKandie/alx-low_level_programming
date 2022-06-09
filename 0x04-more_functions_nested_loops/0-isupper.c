16d810c216e71f13d301d3eaa2f5616a5954d3c7
faveKandie <obianujuofili03@gmail.com>
2022-06-09 04:44:51 -0700

    ghosted

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _isupper - a function that checks for uppercase characte
+ * @c: the character being used
+ * @n: the character being used too
+ * Return: Always 0 Success
+ */
+int _isupper(int c)
+{
+	int n;
+
+	if (n = 'A'; n <= 'Z'; n++)
+		return (1);
+	else
+		return (0);
+}
