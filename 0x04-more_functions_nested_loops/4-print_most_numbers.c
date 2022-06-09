4d3eb9c89d49a4e89e4eabc11114f5c1e92509f3
faveKandie <obianujuofili03@gmail.com>
2022-06-09 06:50:55 -0700

    babe

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,7 +2,6 @@
 
 /**
  * print_most_numbers - prints the numbers from 0 - 9
- * Return: Always 0
  */
 void print_most_numbers(void)
 {
@@ -10,7 +9,7 @@ void print_most_numbers(void)
 
 	for (n = 0; n <= 9; n++)
 	{
-		while(n != 2 && n != 4)
+		if (n != 2 && n != 4)
 			_putchar((n % 10) + '0');
 	}
 		_putchar('\n');
