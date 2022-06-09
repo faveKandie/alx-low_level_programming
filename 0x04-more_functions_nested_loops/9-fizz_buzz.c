30a25f9ab1ecd419714a3b359cce0ec32ce46de0
faveKandie <obianujuofili03@gmail.com>
2022-06-09 10:15:00 -0700

    your

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,7 +9,7 @@ int main(void)
 {
 	int n;
 
-	for (n = 0; n <= 100; n++)
+	for (n = 1; n <= 100; n++)
 	{
 		if ((n % 3) == 0 && (n % 5) == 0)
 			printf("FizzBuzz");
@@ -25,5 +25,6 @@ int main(void)
 		printf(" ");
 	}
 	printf("\n");
+
 	return (0);
 }
