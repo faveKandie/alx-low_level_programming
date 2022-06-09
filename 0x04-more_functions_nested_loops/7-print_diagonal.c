07f97f2bb7b073c75507841c202d7edc6e32e05f
faveKandie <obianujuofili03@gmail.com>
2022-06-09 08:44:46 -0700

    sluise

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -2,17 +2,24 @@
 
 /**
  * print_diagonal - draws a diagonal line on the terminal
- * @n: character used
+ * @n: character printed
  */
 void print_diagonal(int n)
 {
-	int m;
+	int m, p;
 
 	if (n > 0)
 	{
 		for (m = 0; m < n; m++)
-			_putchar('\');
-	}
+		{
+			for (p = 0; p < m; p++)
+				_putchar(' ');
+			_putchar('\\');
 
+			if (len == n - 1)
+				continue;
+			_putchar('\n');
+		}
+	}
 	_putchar('\n');
 }
