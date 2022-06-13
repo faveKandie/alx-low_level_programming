3c5d2e34c009ad2ed1918c689e6d22342eeaf0eb
faveKandie <obianujuofili03@gmail.com>
2022-06-13 09:05:06 -0700

    jesus

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -10,8 +10,7 @@ int _atoi(char *s)
 	int sign = 1;
 	unsigned int num = 0;
 
-	do 
-	{
+	do {
 		if (*s == '-')
 			sign *= -1;
 
@@ -21,8 +20,7 @@ int _atoi(char *s)
 		else if (num > 0)
 			break;
 
-	}
-	while (*s++);
+	} while (*s++);
 
 	return (num * sign);
 }
