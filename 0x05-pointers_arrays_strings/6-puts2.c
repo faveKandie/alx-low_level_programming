91526035bfd1217f43edb1f0db59fea83835afe4
faveKandie <obianujuofili03@gmail.com>
2022-06-13 07:54:49 -0700

    pretty

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * puts2 - Entry point
+ * @str: character being used
+ * Return: Always 0 Success
+ */
+void puts2(char *str)
+{
+	int i, j;
+	i = 0;
+
+	while (s[i] != '\n')
+		i++
+			i--;
+
+	for (j = 0; j <= i; j++)
+	{
+		if (j % 2 == 0)
+			_putchar(str[j]);
+	}
+
+	_putchar('\n');
+}
