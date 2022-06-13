2dfcf62105aa773f39af2fa4176a8f40456f96ba
faveKandie <obianujuofili03@gmail.com>
2022-06-13 08:45:06 -0700

    friends

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -10,10 +10,9 @@ char *_strcpy(char *dest, char *src)
 {
 	int index = 0;
 
-	while (src[index])
+	for (; src[index] != '\0'; index++)
 	{
 		dest[index] = src[index];
-		index++;
 	}
 	return (dest);
 }
