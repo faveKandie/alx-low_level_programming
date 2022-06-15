ad016e085fcfa412cd5b183870d0f3f0c0741d6b
faveKandie <obianujuofili03@gmail.com>
2022-06-15 16:13:56 -0700

    promoted

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,19 +4,17 @@
  * _strcmp - Entry point
  * @s1: character being used
  * @s2: second character being used
- * Return: Always j Success
+ * Return: If str1 < str2, the negative difference of the first unmatched characters
+ * If str1 == str2, 0
+ * If str1 > str2, the positive difference of the first unmatched characters
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0, j = 0;
-
-	for (i = 0; s1[i] != '\0' && s2[j] != '\0'; i++)
+	while (*s1 && *s2 && *s1 == *s2)
 	{
-		if (s1[i] != s2[j])
-		{
-			j = s1[i] - s2[j];
-			break;
-		}
+		s1++;
+		s2++;
 	}
-	return (j);
+
+	return (*s1 - *s2);
 }
