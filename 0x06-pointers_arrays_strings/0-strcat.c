3dceea5c4c028f20ad8f9ffe7ba9cab327452289
faveKandie <obianujuofili03@gmail.com>
2022-06-15 10:29:43 -0700

    reigned

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,26 +2,23 @@
 
 /**
  * *_strcat - Entry point
- * @*dest: character being tested
- * @*src: second character being tested
+ * @dest: character being used
+ * @src: second character
  * Return: Always 0 Success
  */
 char *_strcat(char *dest, char *src)
 {
 	int i, j;
-
 	i = 0;
+	j = 0;
 
 	while (dest[i] != '\0')
 	{
 		i++;
 	}
 
-	j = 0;
-
 	while (src[j] != '\0')
 	{
-		dest[i] = src[j];
 		j++;
 		i++;
 	}
