651c44a5a6ec015e5dfe1ae325ae6834e23abfd0
faveKandie <obianujuofili03@gmail.com>
2022-06-15 15:32:18 -0700

    heist commit

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -11,21 +11,13 @@ char *_strncat(char *dest, char *src, int n)
 {
 	int i, j;
 	i = 0;
-
-	while (dest[i] != '\0')
-	{
-		i++;
-	}
-
 	j = 0;
 
-	while (src[j] != '\0' && j < n)
-	{
-		dest[i] = src[j];
+	while (dest[i++])
 		j++;
-		i++;
-	}
 
-	dest[i] != '\0';
+	for (i = 0; src[i] && i < n; i++)
+		dest[j] = src[i];
+
 	return (dest);
 }
