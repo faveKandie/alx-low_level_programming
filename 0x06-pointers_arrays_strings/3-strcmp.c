e94ae7b4a4c9316add429b347d68b6f740fcc743
faveKandie <obianujuofili03@gmail.com>
2022-06-15 07:24:47 -0700

    fetchy

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strcmp - Entry point
+ * @*s1: character being used
+ * @*s2: second character being used
+ * Return: Always 0 Success
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i, j;
+
+	j = 0;
+
+	for (i = 0; s1[i] != '\0' && s2[j] != '\0'; i++)
+	{
+		if (s1[i] != s2[j])
+		{
+			j = s1[i] - s2[j];
+			break;
+		}
+	}
+	return (j);
+}
