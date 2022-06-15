ee626348e733a7d8cb450e9a95be8586e4cb8369
faveKandie <obianujuofili03@gmail.com>
2022-06-15 16:18:06 -0700

    bettybutter

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,9 +4,7 @@
  * _strcmp - Entry point
  * @s1: character being used
  * @s2: second character being used
- * Return: If str1 < str2, the negative difference of the first unmatched characters
- * If str1 == str2, 0
- * If str1 > str2, the positive difference of the first unmatched characters
+ * Return: If str1 < str2, the negative difference of the first unmatched characters, If str1 == str2, 0 and if str1 > str2, the positive difference of the first unmatched characters
  */
 int _strcmp(char *s1, char *s2)
 {
