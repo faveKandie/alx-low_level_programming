083b56636bbe91e49e5ae236882d19dfee60417f
faveKandie <obianujuofili03@gmail.com>
2022-06-15 16:04:02 -0700

    thankful

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -2,15 +2,13 @@
 
 /**
  * _strcmp - Entry point
- * @*s1: character being used
- * @*s2: second character being used
- * Return: Always 0 Success
+ * @s1: character being used
+ * @s2: second character being used
+ * Return: Always j Success
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i, j;
-
-	j = 0;
+	int i = 0, j = 0;
 
 	for (i = 0; s1[i] != '\0' && s2[j] != '\0'; i++)
 	{
