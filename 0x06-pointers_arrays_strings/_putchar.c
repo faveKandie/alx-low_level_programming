084ea91819ae90f3359144530a92f4b0bb34e0a2
faveKandie <obianujuofili03@gmail.com>
2022-06-15 11:44:00 -0700

    fush

--- 0x06-pointers_arrays_strings/_putchar.c
+++ 0x06-pointers_arrays_strings/_putchar.c
@@ -4,7 +4,9 @@
 /**
  * _putchar - writes the character c to stdout
  * @c: The character to print
- * Return: either 0 or 1
+ *
+ * Return: On success 1
+ * On error, -1 is returned, and errno is set appropriately
  */
 int _putchar(char c)
 {
