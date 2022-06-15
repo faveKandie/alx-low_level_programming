d8ca0eeba8235784e7bb6ca9495b177f44bf1ab5
faveKandie <obianujuofili03@gmail.com>
2022-06-15 12:01:52 -0700

    taste

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,22 +8,13 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int i, j;
-	i = 0;
-	j = 0;
+	int index = 0, dest_len = 0;
 
-	while (dest[i] != '\0')
-	{
-		i++;
-	}
+	while (dest[index++])
+		dest_len++;
 
-	while (src[j] != '\0')
-	{
-		dest[i] = src[j];
-		j++;
-		i++;
-	}
+	for (index = 0; src[index]; index++)
+		dest[dest_len++] = src[index];
 
-	dest[i] != '\0';
 	return (dest);
 }
