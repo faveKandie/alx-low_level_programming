252ff029769836591501d36dc9f56c4fdec03a13
faveKandie <obianujuofili03@gmail.com>
2022-06-15 17:20:17 -0700

    trial

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,27 +4,16 @@
  * _strcmp - Compares pointers to two strings
  * @s1: A pointer to the first string to be compared
  * @s2: A pointer to the second string to be compared
- * Return: Always value 
+ * Return: Always 0 Success
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i, value;
+	int i;
 
-	i = 0;
-
-	while (s1[i] != '\0')
+	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
 	{
-		if (s1[i] == s2[i])
-		{
-			value = s1[i] - s2[i];
-			i++;
-		}
-		else
-		{
-			value = s1[i] - s2[i];
-			break;
-		}
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
 	}
-
-	return (value);
+	return (0);
 }
