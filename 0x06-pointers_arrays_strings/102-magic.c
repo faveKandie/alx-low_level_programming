3bc21814b2f422580db340c205762377b31c9db3
faveKandie <obianujuofili03@gmail.com>
2022-06-16 04:31:45 -0700

    turtles

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -10,10 +10,10 @@ int main(void)
 	p = &n;
 	/*
 	 * write your line of code here...
-	 * Remember:
+	 * main - Entry point
 	 * - you are not allowed to use a
 	 *   - you are not allowed to modify p
-	 *   - only one statement
+	 *   Return: Always 0 Success
 	 *   - you are not allowed to code anything else than this line of code
 	 */
 	*(p + 5) = 98;
