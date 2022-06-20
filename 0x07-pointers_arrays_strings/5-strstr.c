04875111a0e8d52f152d7c407181ac90b8defcd0
faveKandie <obianujuofili03@gmail.com>
2022-06-20 05:34:15 -0700

    identity

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -4,7 +4,7 @@
  * _strstr - a function that locates a substring
  * @haystack: string to check 
  * @needle: substring to find 
- * Return: a pointer to the beginning of the located substring, or NULL if the substring is not found
+ * Return: a pointer to the beginning of the located substring, or NULL 
  */
 char *_strstr(char *haystack, char *needle)
 {
