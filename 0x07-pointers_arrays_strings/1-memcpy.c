fe891c97887ba4561f409920c5f4aaaa4cfb5dc4
faveKandie <obianujuofili03@gmail.com>
2022-06-20 04:58:21 -0700

    semmee

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,11 +1,10 @@
 #include "mai.h"
 
 /**
- * _memcpy - a function that copies memory area
- * @dest: memory are<F6>a to be filled
+ * _memcpy - copies memory area
+ * @dest: memory area to be filled
  * @src: memory area being copied
  * @n: number of area to be filled
- *
  * Return: pointter to memory dest
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
