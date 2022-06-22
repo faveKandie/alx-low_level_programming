591cd36c8ad5ceb98b8fa7191c624e736df6ca61
faveKandie <obianujuofili03@gmail.com>
2022-06-22 00:47:55 -0700

    powrecursionc

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _pow_recursion - a function that returns the value of x raised to the power of y
+ * _pow_recursion - a function that returns x raised to y
  * @x: value used
  * @y: power raised
  * Return: x to the power of y success
@@ -12,5 +12,5 @@ int _pow_recursion(int x, int y)
 		return (-1);
 	if (y == 0)
 		return (1);
-	return (x * _pow_recursion( x, y - 1));
+	return (x * _pow_recursion(x, y - 1));
 }
