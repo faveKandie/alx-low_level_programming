3920d571feeb3874a153f2b3570e2ab0ed42db1f
faveKandie <obianujuofili03@gmail.com>
2022-06-24 04:14:09 -0700

    argumen arg

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,7 +7,7 @@
  * @argc: number of arguments passed to the function
  * Return: Always 0 Success
  */
-int main (int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
