d084103699cfc890f08207fb6d5a134cadd12bef
faveKandie <obianujuofili03@gmail.com>
2022-06-27 22:11:34 -0700

    reachout

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -27,23 +27,23 @@ char *str_concat(char *s1, char *s2)
   char *m;
 
   if (s1 == NULL)
-  s1 = "\0";
+	s1 = "\0";
 
   if (s2 == NULL)
-  s2 = "\0";
+	s2 = "\0";
 
   size1 = _strlen(s1);
   size2 = _strlen(s2);
   m = malloc((size1 + size2) *sizeof(char) + 1);
   if (m == 0)
-  return (0);
+	return (0);
 
   for (i = 0; i <= size1 + size2; i++)
   {
-  if (i < size1)
-  m[i] = s1[i];
-  else
-  m[i] = s2[i - size1];
+	if (i < size1)
+		m[i] = s1[i];
+	else
+		m[i] = s2[i - size1];
   }
   m[i] = '\0';
   return (0);
