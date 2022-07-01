c6036d7069ff145e98c4a2841c96e954f9c7c2fe
faveKandie <obianujuofili03@gmail.com>
2022-06-30 19:47:52 -0700

    jesus

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -8,7 +8,6 @@ int *array_range(int min, int max);
 void *_calloc(unsigned int nmemb, unsigned int size);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *malloc_checked(unsigned int b);
-int lenght(char **DoblePointer);
 int _putchar(char c);
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
