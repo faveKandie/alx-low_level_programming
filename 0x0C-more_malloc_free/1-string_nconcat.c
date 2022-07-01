4d3e4d4fe25493a59cff54683ee16fcca366f793
faveKandie <obianujuofili03@gmail.com>
2022-06-30 19:34:31 -0700

    loot

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -4,7 +4,7 @@
 /**
  * string_nconcat - concatenates two strings
  * @s1: first string
- * @s2 - second string
+ * @s2: second string
  * @n: number of bytes of s2
  * Return: pointer Success, NULL failed
  */
