be748cda18052c2bdb8cb2ad363ce1a783c35a19
faveKandie <obianujuofili03@gmail.com>
2022-06-30 17:25:38 -0700

    full

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -28,18 +28,25 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	if (s1 == NULL)
 		s1 = "";
+
 	if (s2 == NULL)
 		s2 = "";
+
 	if (n < _strlen(s2))
 		m = malloc(_strlen(s1) + n * sizeof(char) + 1);
 	else
 		m = malloc(_strlen(s1) + _strlen(s2) + 1);
+
 	if (m == 0)
 		return (NULL);
+
 	for (i = 0; s1[i] != '\0'; i++)
 		m[i] = s1[i];
+
 	for (j = 0; s2[j] != '\0' && j < n; i++, j++)
 		m[i] = s2[j];
+
 	m[i] = '\0';
+
 	return (m);
 }
