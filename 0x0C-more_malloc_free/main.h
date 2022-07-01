9501b275ecd3583b93a7ec8b5babd6646a6751c5
faveKandie <obianujuofili03@gmail.com>
2022-07-01 10:19:39 -0700

    forever

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,8 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#define min(x, y) (((x) < (y)) ? (x) : (y))
-
 int _putchar(char c);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
