2a61b9b86b0ee66507184d993efb83c84d096527
faveKandie <obianujuofili03@gmail.com>
2022-06-30 17:35:54 -0700

    daisy

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,19 +1,6 @@
 #include "main.h"
 #include <stdlib.h>
-/**
- * _strlen - find length of a string
- * @s: string
- * Return: int
- */
-unsigned int _strlen(char *s)
-{
-	unsigned int size = 0;
-
-	for (; s[size] != '\0'; size++)
-		;
-	return (size);
-}
-
+#include <stdio.h>
 /**
  * string_nconcat - concatenates two strings
  * @s1: first string
@@ -23,30 +10,35 @@ unsigned int _strlen(char *s)
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int i, j;
-	char *m;
+	char *arr;
+	unsigned int i, j, co, co_2;
 
 	if (s1 == NULL)
 		s1 = "";
-
 	if (s2 == NULL)
 		s2 = "";
 
-	if (n < _strlen(s2))
-		m = malloc(_strlen(s1) + n * sizeof(char) + 1);
-	else
-		m = malloc(_strlen(s1) + _strlen(s2) + 1);
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+	}
 
-	if (m == 0)
+	if (n < j)
+		j = n;
+
+	j += i;
+	arr = malloc(sizeof(char *) * (j + 1));
+
+	if (arr == NULL)
 		return (NULL);
 
-	for (i = 0; s1[i] != '\0'; i++)
-		m[i] = s1[i];
-
-	for (j = 0; s2[j] != '\0' && j < n; i++, j++)
-		m[i] = s2[j];
-
-	m[i] = '\0';
-
-	return (m);
+	for (co = 0; co < i; co++)
+		arr[co] = s1[co];
+	for (co_2 = 0; co < j; co_2++)
+	{
+		arr[co] = s2[co_2];
+		co++;
+	}
+	co++;
+	arr[co] = '\0';
+	return (arr);
 }
